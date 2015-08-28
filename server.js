6b34c3f5799f5b267c06ea5b42a45992e805fc4e
Joseph Tsui <liuhui@RD-TsuiMingHei-IMAC.local>
2015-08-28 11:09:57 +0800

    changing to hello azure

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 8081;
 
 app.use(express.static(__dirname + '/public'));
 
